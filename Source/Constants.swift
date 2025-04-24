52d3499a8b643ecc7572c3d4c800e66a7aa9f7d0
Jon Schneider <jon@jonschneider.me>
2025-04-24 12:39:31 -0400

    Updates row heights to be responsive to system font size

--- Source/Constants.swift
+++ Source/Constants.swift
@@ -11,5 +11,10 @@ import UIKit
 internal struct Constants {
     internal static let TEXT_FIELD_HSPACE: CGFloat = 6.0
     internal static let MINIMUM_TEXTFIELD_WIDTH: CGFloat = 56.0
-    internal static let STANDARD_ROW_HEIGHT: CGFloat = 25.0
+
+    private static let BASELINE_ROW_HEIGHT: CGFloat = 25.0
+    private static let bodyFontInstance = UIFont.preferredFont(forTextStyle: .body)
+    internal static var STANDARD_ROW_HEIGHT: CGFloat {
+        return max(BASELINE_ROW_HEIGHT, BASELINE_ROW_HEIGHT * (bodyFontInstance.pointSize / 17.0)) // 17 is the standard body font size
+    }
 }
